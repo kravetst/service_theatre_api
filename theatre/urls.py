75957cf256a4c72a74d51b324a0be984f2f0b274
kravetst <taraskravetc28@gmail.com>
2025-09-20 14:34:11 +0300

    Update gitignore, urls, requirements and IDE settings

---
+++ theatre/urls.py
@@ -0,0 +1,23 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from theatre.views import (
+    PlayViewSet,
+    ActorViewSet,
+    GenreViewSet,
+    TheatreHallViewSet,
+    PerformanceViewSet,
+    ReservationViewSet
+)
+
+app_name = "theatre"
+router = DefaultRouter()
+router.register("plays", PlayViewSet)
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+router.register("theatre_halls", TheatreHallViewSet)
+router.register("performances", PerformanceViewSet)
+router.register("reservations", ReservationViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
