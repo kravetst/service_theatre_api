e6caad04869d1ab49b41ec0f10ba2c4fb43169a7
kravetst <taraskravetc28@gmail.com>
2025-09-24 13:07:28 +0300

    Update settings and theatre app configuration

--- config/settings.py
+++ config/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "user",
     "theatre",
     "drf_spectacular",
-    "user",
 ]
 
 MIDDLEWARE = [
@@ -146,3 +146,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+}
